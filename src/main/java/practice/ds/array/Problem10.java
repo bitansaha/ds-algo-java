d05ff08f8ee41e57f1d6a3a9b5c2f7954b5dfc4d
Bitan Saha <bitansaha.1@gmail.com>
2022-06-05 00:19:34 -0600

    Count of possible rotations of given Array to remove largest element from first half

---
+++ src/main/java/practice/ds/array/Problem10.java
@@ -0,0 +1,40 @@
+package practice.ds.array;
+
+public class Problem10 {
+
+    public static int findNumberOfCyclicShifts(int[] array) {
+        Integer maxValue = null;
+        int initialMaxValueIndex = -1;
+        int maxMaxValueIndex = -1;
+
+        for (int index = 0; index < array.length; index++) {
+            if (maxValue == null) {
+                maxValue = array[index];
+                maxMaxValueIndex = initialMaxValueIndex = index;
+            } else {
+                int currentValue = array[index];
+                if (currentValue == maxValue) {
+                    maxMaxValueIndex = index;
+                } else if (currentValue > maxValue) {
+                    maxValue = currentValue;
+                    maxMaxValueIndex = initialMaxValueIndex = index;
+                }
+            }
+        }
+
+        if (maxValue != null
+                && ((maxMaxValueIndex - initialMaxValueIndex) < array.length/2)
+                && (initialMaxValueIndex < array.length/2)) {
+            int leftMinRotation = array.length/2 - initialMaxValueIndex;
+            int rightMaxRotation = array.length - 1 - maxMaxValueIndex;
+            return Math.max(leftMinRotation, rightMaxRotation);
+        } else {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{3, 3, 5, 3, 3, 3};
+        System.out.println(findNumberOfCyclicShifts(array));
+    }
+}
